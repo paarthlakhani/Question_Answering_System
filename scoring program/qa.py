028cefa9f73d7330a797221ae5a20ff10af9466b
paarthlakhani <paarth.lakhani@utah.edu>
2020-11-03 12:18:54 -0700

    Word match function

--- scoring program/qa.py
+++ scoring program/qa.py
@@ -1,7 +1,11 @@
 import sys
+
+import nltk
 from nltk.stem.porter import PorterStemmer
 from processQuestions import findQuestionType
 from stopWordsHandling import removeStopWords
+from nltk.tokenize import sent_tokenize
+
 '''
     Process the input file
     inputFile: inputfile from 'qa <inputfile>' command passed from command line
@@ -9,6 +13,8 @@ from stopWordsHandling import removeStopWords
     subsequent lines: story Ids
     Return: directory path as a string and story ids as a list
 '''
+
+
 def processInputFile(inputFile):
     with open(inputFile) as fp:
         lines = fp.readlines()
@@ -16,46 +22,85 @@ def processInputFile(inputFile):
         storyIds = [storyId.strip("\n").strip(" ") for storyId in lines[1:]]
         return dirPath, storyIds
 
-def processStory(story):
-    headline = story[0].strip()
-    date = story[1].strip()
-    story_id = story[2].strip()
-    for story_line in range(3, len(story)):
-        story_line = story[story_line].strip()
-        story_line_words = story_line.split()
-        for word in story_line_words:
-            porter_stemmer = PorterStemmer()
-            stem = porter_stemmer.stem(word)
-            # print("Word is: " + word, "Stem is: " + stem)
+
+def processStory(story_lines):
+    story_sentences = []
+    story_headline = story_lines[0].strip()
+    story_date = story_lines[1].strip()
+    story_id = story_lines[2].strip()
+
+    story_line_no = 3
+    while story_lines[story_line_no].strip() != "TEXT:":
+        story_line_no = story_line_no + 1
+    story_line_no = story_line_no + 1
+    story = ''.join(story_lines[story_line_no:])
+    story_sentences = sent_tokenize(story)
+    return story_sentences
+
+
+def morphological_roots(list_of_words):
+    porter_stemmer = PorterStemmer()
+    for word_index in range(0, len(list_of_words)):
+        list_of_words[word_index] = porter_stemmer.stem(list_of_words[word_index])
+    return list_of_words
+
+
+def word_match(morphological_root_of_question, story_sentence):
+    score = 0
+    story_sentence_without_stop_words = removeStopWords(story_sentence)
+    tokenized_story_words = nltk.word_tokenize(story_sentence_without_stop_words)
+    morph_story_words = morphological_roots(tokenized_story_words)
+    for morph_story_word in morph_story_words:
+        if morph_story_word in story_sentence_without_stop_words:
+            score = score + 1
+    return score;
+
+
+def processQuestions(questionsFile, story_sentences):
+    answer = ""
+    question_id = ""
+    with open(questionsFile) as fp:
+        questions = fp.readlines()
+        for question_component in questions:
+            if question_component != "\n":
+                question_component = question_component.strip("\n")
+                question_type_data = question_component.split(":")
+                if question_type_data[0] == "QuestionID":
+                    question_id = question_type_data[1]
+                if question_type_data[0] == "Question":
+                    sentence_score = {}
+                    question = question_type_data[1]
+                    question_type = findQuestionType(question.lower())
+                    question_without_stop_words = removeStopWords(question)
+                    question_words = nltk.word_tokenize(question_without_stop_words)
+                    morphological_root_of_question = morphological_roots(question_words)
+                    for story_sentence_index in range(0, len(story_sentences)):
+                        word_match_score = word_match(morphological_root_of_question, story_sentences[story_sentence_index])
+                        sentence_score[story_sentence_index] = word_match_score
+                    print(sentence_score)
+            else:
+                question_id = "QuestionID:" + str(question_id) + "\n"
+                answer = "Answer:" + answer + "\n"
+                question_id_answer = question_id + answer
+                print(question_id_answer)
+                answer = ""
+                question_id = ""
+
 
 '''
     questionsFile: File that has a list of questions need to be answered
     storyFile: File name that has the story for the questions listed in questionsFile
     Return: None
 '''
-def findQuestionsAndStory(questionsFile, storyFile):
-    try:
-        with open(storyFile) as story:
-            processStory(story.readlines())
-        #with open(questionsFile) as fp:
-        #    print(fp.readlines())
-    except:
-        pass
-        # print("File "+questionsFile+" doesn't exist!")
 
-    with open(questionsFile) as fp:
-        questionFileContents = fp.readlines()
-    for line in questionFileContents:
-        # If not a blank line
-        if line.strip("\n"):
-            lineContent = line.split(":")
-            tag, content = lineContent[0], lineContent[1].strip("\n")
-            if tag == "Question":
-                # print(content)
-                questionType = findQuestionType(content.lower())
-                questionWithoutStopWords = removeStopWords(content)
-                # print(questionType,"******", questionWithoutStopWords)
-                # print("____________________________________________________________")
+
+def find_questions_and_story(questionsFile, storyFile):
+    with open(storyFile) as story:
+        story_sentences = processStory(story.readlines())
+        #print(story_sentences)
+    # with open(questionsFile) as fp:
+    #    print(fp.readlines())
+    processQuestions(questionsFile, story_sentences)
 
 '''
     dirPath: a string containing directory path to all the stories
@@ -63,11 +108,14 @@ def findQuestionsAndStory(questionsFile, storyFile):
     Return: None
     For each story Id, find the story file and the question File, then process that story to answer all the questions.
 '''
+
+
 def findStories(dirPath, storyIdLst):
     for storyId in storyIdLst:
-        storyFilePath = dirPath+storyId+".story"
-        storyQuestionFilePath = dirPath+storyId+".questions"
-        findQuestionsAndStory(storyQuestionFilePath, storyFilePath)        
+        storyFilePath = dirPath + storyId + ".story"
+        storyQuestionFilePath = dirPath + storyId + ".questions"
+        find_questions_and_story(storyQuestionFilePath, storyFilePath)
+
 
 if __name__ == "__main__":
     inputFile = sys.argv[1]
--- scoring program/stopWordsHandling.py
+++ scoring program/stopWordsHandling.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.corpus import stopwords
+
 stopWords = set(stopwords.words('english'))
 
 '''
@@ -7,8 +8,8 @@ stopWords = set(stopwords.words('english'))
     Return: a string without stop words
     This function simply checks if the current word in belongs to stop words set. If not it adds it to te output list.
 '''
+
 def removeStopWords(sentence):
     wordsInSentence = nltk.word_tokenize(sentence)
     sentenceWithoutStopWords = " ".join([word for word in wordsInSentence if word not in stopWords])
     return sentenceWithoutStopWords
-    
